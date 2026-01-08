b31791b17620889e88822236d12fc1158551d5ed
koenmoret <koen@moret.info>
2026-01-08 19:35:29 +0100

    feat: add PlantSoort entity

--- src/main/java/nl/novi/plantenkennis/entity/PlantSoort.java
+++ src/main/java/nl/novi/plantenkennis/entity/PlantSoort.java
@@ -1,4 +1,27 @@
 package nl.novi.plantenkennis.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "plant_soorten")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PlantSoort {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String naam;
+
+    @Column(length = 120)
+    private String latijnseNaam;
+
+    @Column(length = 2000)
+    private String omschrijving;
 }
