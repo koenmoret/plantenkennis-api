fe85911176e4039189fa757ef67bf0e7c1340791
koenmoret <koen@moret.info>
2026-01-08 16:20:44 +0100

    chore: bootstrap spring boot maven project

---
+++ src/test/java/nl/novi/plantenkennis/PlantenkennisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.plantenkennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantenkennisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
